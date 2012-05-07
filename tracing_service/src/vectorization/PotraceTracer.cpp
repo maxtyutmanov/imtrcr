34bfb984dfc308b9fd3813b29ad8c56ec708e866
MaksimDT <tyutmanovmd@gmail.com>
2012-05-07 18:13:07 +0400

    Finished with stub of tracing service.

--- tracing_service/src/vectorization/PotraceTracer.cpp
+++ tracing_service/src/vectorization/PotraceTracer.cpp
@@ -1,15 +1,36 @@
 #pragma once
 
 #include "vectorization/PotraceTracer.h"
+#include <imaging/Path.h>
 
 using namespace ImTrcr::Imaging;
+using namespace ImTrcr::Imaging::Primitives;
 
 namespace ImTrcr {
 namespace Vectorization {
 
-  Imaging::VectorImage* PotraceTracer::Trace(const Imaging::RasterImage& rasterImage) {
-    return new VectorImage();
-  }
+    Imaging::VectorImage* PotraceTracer::Trace(const Imaging::RasterImage& rasterImage) {
+        VectorImage* vectorImage = new VectorImage();
 
+        PathCommand moveTo = PathCommand::MoveToCommand(10, 10.2);
+        PathCommand bezier = PathCommand::QuadraticBezierToCommand(20.123, 20, 30.345, 30.1);
+        PathCommand close = PathCommand::ClosePathCommand();
+        Path* path = new Path(ArgbQuad(0, 0, 0));
+        path->AddCommand(moveTo);
+        path->AddCommand(bezier);
+        path->AddCommand(close);
+        vectorImage->AddPrimitive(path);
+
+        moveTo = PathCommand::MoveToCommand(100, 100.2);
+        bezier = PathCommand::QuadraticBezierToCommand(200.123, 200, 300.345, 300.1);
+        close = PathCommand::ClosePathCommand();
+        path = new Path(ArgbQuad(0, 0, 0));
+        path->AddCommand(moveTo);
+        path->AddCommand(bezier);
+        path->AddCommand(close);
+        vectorImage->AddPrimitive(path);
+
+        return vectorImage;
+    }
 }
 }
